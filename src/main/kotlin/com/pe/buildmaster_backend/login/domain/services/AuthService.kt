1f63ea9c6f0ef34cc1d73aa7c0cce31eccc9f796
Alex <alexcantsed@hotmail.com>
2025-05-13 10:06:41 -0500

    Revert "Merge pull request #8 from 1ACC0238-2510-358-BuildMaster/feature/login"
    This reverts commit fffe446928051b329d2bd6eb2f5f11ffcf171362, reversing
    changes made to e01b328682b1db9e1b41601a60d5999e309b93de.

--- src/main/kotlin/com/pe/buildmaster_backend/login/domain/services/AuthService.kt
+++
@@ -1,8 +0,0 @@
-package com.pe.buildmaster_backend.login.domain.services
-
-import com.pe.buildmaster_backend.login.domain.model.valueobjects.User
-
-interface AuthService {
-    fun login(email: String, password: String): String
-    fun registrar(email: String, password: String, nombre: String): User
-}
--- src/main/kotlin/com/pe/buildmaster_backend/login/infrastructure/persistence/jpa/repositories/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.pe.buildmaster_backend.login.infrastructure.persistence.jpa.repositories
-
-import com.pe.buildmaster_backend.login.domain.model.valueobjects.User
-
-interface UserRepository {
-    fun findByEmail(email: String): User?
-    fun save(usuario: User): User
-}
