8f4af95345a1efe01c1f8bb7846a30d251595fee
Russell Stephen Romero Qwistgaard <u202211043@upc.edu.pe>
2025-06-15 13:38:08 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5") // For JSON parsing
 	runtimeOnly("org.postgresql:postgresql")
 
 	compileOnly("org.projectlombok:lombok")
