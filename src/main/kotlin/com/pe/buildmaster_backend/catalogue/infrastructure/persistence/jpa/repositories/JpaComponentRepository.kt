e0c988ac79bd4734581d1b9971114829baa6d42a
Alex <alexcantsed@hotmail.com>
2025-06-16 21:56:35 -0500

    hotfix: for searchname

--- src/main/kotlin/com/pe/buildmaster_backend/catalogue/infrastructure/persistence/jpa/repositories/JpaComponentRepository.kt
+++ src/main/kotlin/com/pe/buildmaster_backend/catalogue/infrastructure/persistence/jpa/repositories/JpaComponentRepository.kt
@@ -7,10 +7,15 @@ import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface JpaComponentRepository : JpaRepository<Component, Long>{
-    @Query("SELECT c FROM Component c WHERE (:type IS NULL OR c.type LIKE %:type%) " +
-            "AND (:categoryId IS NULL OR c.category.id = :categoryId) " +
-            "AND (:manufacturerId IS NULL OR c.manufacturer.id = :manufacturerId)")
+interface JpaComponentRepository : JpaRepository<Component, Long> {
+
+    @Query("""
+        SELECT c FROM Component c 
+        WHERE (:name IS NULL OR LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%')))
+          AND (:type IS NULL OR LOWER(c.type) LIKE LOWER(CONCAT('%', :type, '%')))
+          AND (:categoryId IS NULL OR c.category.id = :categoryId)
+          AND (:manufacturerId IS NULL OR c.manufacturer.id = :manufacturerId)
+    """)
     fun search(
         @Param("name") name: String?,
         @Param("type") type: String?,
